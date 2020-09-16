d87ce959c4f4db30dfe3a870ce67e9b0d63e52b8
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-16 02:03:42 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -53,7 +53,7 @@ end
 
 def add_s(array)
    
-  array.each_with_index.collect do |words, index|
+  array.each_with_index.collect do |words, index.reject[2]|
    words.insert(-1, "s")
   end 
  array
